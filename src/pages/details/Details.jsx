81fed37cd643564a196b1c2dea949338e6cf121d
Nageshwar1997 <nageshpawarpatil@gmail.com>
2024-03-07 19:41:24 +0530

    Movie Details Banner Section Added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "./style.scss";
 
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+
 const Details = () => {
-  return <div>Details</div>;
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
+
+
+  return <div>
+    <DetailsBanner video = {data?.results?.[0]} crew = {credits?.crew} />
+  </div>;
 };
 
 export default Details;
